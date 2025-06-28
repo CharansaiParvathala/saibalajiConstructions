3cf7fc98e6e7369a92eb6b9b7c8c4c2de87a5b2e
Charansai Parvathala <charansaiparvathala@gmail.com>
2025-06-28 08:50:46 +0530

    server errr2

---
+++ api/index.ts
@@ -0,0 +1,6 @@
+// This file serves as the entry point for Vercel serverless functions
+// It imports and exports the Express app from the server directory
+
+import app from '../server/index';
+
+export default app; 
